7adfa5a7dcc1db1e861c75baf01e61867d035f77
qiuyuhan <1031893464@qq.com>
2018-09-26 11:12:16 +0800

    commit1

---
+++ mvc5test1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc5test1.Models;
+namespace mvc5test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int ? id)
+        {
+            List<User> users = new List<User>()
+            {
+                new User(){ id=0,name="xxx",age=20},
+                 new User(){ id=10,name="xxasas",age=21}
+            };
+            ViewBag.jsdata = "张三";
+            return View(users);   //强类型视图(控制器向视图返回一个指定类型对象)
+        }
+        public ActionResult Logi()
+        {
+            
+            return View(new Models.User());
+        }
+        [HttpPost]
+        public ActionResult Logi(Models.User u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                string userName = "abcdef";
+                if (u.name == userName)
+                {
+                    //手动添加
+                    ModelState.AddModelError("DoubleUser", "该用户名已存在！");
+                    return View(u);
+                }
+            }
+            else
+            {
+
+            }
+            return View(u);
+        }
+       
+    }
+}
---
+++ mvc5test1/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace mvc5test1.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class User
+    {
+        public int id { get; set; }
+        [DisplayName("年龄")]
+        [Range(0,120,ErrorMessage ="{0}必须在{1}和{2}之间")]
+        public int age { get; set; }
+
+
+        [DisplayName("用户名")]
+        [Required(ErrorMessage = "{0}不能为空")]
+        public string name { get; set; }
+
+
+        [DisplayName("用户编码")]
+        [Required(ErrorMessage ="{0}不能为空")]
+        [StringLength(10,MinimumLength =6,ErrorMessage ="{0}的长度不能超过{1},也不能低于{2}")]
+        public string userCode { get; set; }
+
+        [DisplayName("注册日期")]
+        [Range(typeof(DateTime),"2000-01-01","2018-09-19",ErrorMessage ="{0}必须在{1}和{2}之间")]
+        public DateTime regTime { get; set; }
+
+        [DisplayName("密码")]
+        [RegularExpression(@"[a-zA-Z]{1}\d{5}",ErrorMessage ="格式不符合要求")]
+        public string UserPwd { get; set; }
+
+        [Compare("UserPwd", ErrorMessage ="两次密码不一致")]
+        public string UserPwd2 { get; set; }
+
+    }
+}
