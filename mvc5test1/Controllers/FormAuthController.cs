d64e309aa5e9d68bf793a2cc8eeeeaa5557a89b3
qiuyuhan <1031893464@qq.com>
2018-10-23 11:14:07 +0800

    forms验证

---
+++ mvc5test1/Controllers/FormAuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+/*
+ Forms权限验证
+ 先在Web.config 配置 system.web 下
+  <authentication mode= "Forms">
+  </authentication>
+  导入 System.Web.Security
+  FormsAuthentication.SetAuthCookie("用户名",是否永久保存)
+  this.User.Identity.IsAuthenticated 判断是否存入cookie
+  this.User.Identity.Name 获取刚刚存入的用户名
+  FormsAuthentication.SignOut();清空cookie 
+   */
+namespace mvc5test1.Controllers
+{
+    public class FormAuthController : Controller
+    {
+        // GET: FormAuth
+        public ActionResult Index()
+        {
+            FormsAuthentication.SetAuthCookie("qiuyuhan", true);//true or 2880
+            return Content("登录成功！");
+        }
+        public ActionResult Auths()
+        {
+            if (this.User.Identity.IsAuthenticated)
+             {
+                return Content(this.User.Identity.Name);
+           }
+          else
+          {
+              return     Content("没有登录");
+          }
+
+        }
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();//清空
+            return Content("注销成功");
+        }
+    }
+}
