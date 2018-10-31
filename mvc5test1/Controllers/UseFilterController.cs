7010eb1a230a9f3236967047113d47053db0a9ce
hanhanyh <1031893464@qq.com>
2018-10-31 16:41:48 +0800

    自定义过滤器

---
+++ mvc5test1/Controllers/UseFilterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc5test1.Filters;
+namespace mvc5test1.Controllers
+{
+    public class UseFilterController : Controller
+    {
+        // GET: UseFilter
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 异常日志过滤器测试
+        /// </summary>
+        /// <returns></returns>
+        [LogFilter]
+        public ActionResult Exception()
+        {
+            string s = null;
+            int i = s.Length;
+            return Content("异常过滤器测试！");
+        }
+    }
+}
---
+++ mvc5test1/Filters/LogFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+namespace mvc5test1.Filters
+{
+    /// <summary>
+    /// 自动以日志过滤器,继承FilterAttribute是为了 使用特性，实现IExcepitionFilter是为了使用过滤器
+    /// </summary>
+    public class LogFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            string path = filterContext.HttpContext.Server.MapPath("~/Logs/log.txt");
+            StreamWriter sw = new StreamWriter(path,true,System.Text.Encoding.UTF8);
+            sw.WriteLine(DateTime.Now.ToString() + filterContext.Exception.Message);
+            sw.Flush();
+            sw.Close();
+
+        }
+    }
+}
